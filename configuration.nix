0fc1cad2e69f996856ea063a1b6ca2e7b4d43a22
vaaski <admin@vaa.ski>
2024-03-06 21:52:31 +0100

    remove unnecessary comments

--- configuration.nix
+++ configuration.nix
@@ -51,14 +51,6 @@
     ];
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
   services.openssh = {
     enable = true;
     settings.PasswordAuthentication = false;
@@ -66,11 +58,11 @@
     settings.PermitRootLogin = "no";
   };
 
+  services.pcscd.enable = true;
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
   };
-  services.pcscd.enable = true;
 
   time.timeZone = "Europe/Berlin";
   i18n.defaultLocale = "en_US.UTF-8";
@@ -92,12 +84,7 @@
     xkbVariant = "";
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  # don't change
+  system.stateVersion = "23.11";
 
 }
