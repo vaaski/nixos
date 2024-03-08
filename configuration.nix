874a63b17cd348190e9715019410e6d748f6d9dc
vaaski <admin@vaa.ski>
2024-03-08 13:02:32 +0100

    add starship + pure preset

--- configuration.nix
+++ configuration.nix
@@ -35,6 +35,7 @@ in
     nodejs_20
     nodePackages."@antfu/ni"
     pinentry
+    starship
     wget
     yt-dlp
     (fetchPackage {
@@ -81,6 +82,46 @@ in
       # work mac
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKoiophA8kvDCGunKUiRX91opLWPoNUi+LIsVv+bCmz2"
     ];
+  };
+
+  programs.starship.enable = true;
+  programs.starship.settings = {
+    format = "$username$hostname$directory$git_branch$git_state$git_status$cmd_duration$line_break$python$character";
+    directory = {
+      style = "blue";
+    };
+    character = {
+      success_symbol = "[❯](purple)";
+      error_symbol = "[❯](red)";
+      vimcmd_symbol = "[❮](green)";
+    };
+    git_branch = {
+      format = "[$branch]($style)";
+      style = "bright-black";
+    };
+    git_status = {
+      format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+      style = "cyan";
+      conflicted = "​";
+      untracked = "​";
+      modified = "​";
+      staged = "​";
+      renamed = "​";
+      deleted = "​";
+      stashed = "≡";
+    };
+    git_state = {
+      format = "\([$state( $progress_current/$progress_total)]($style)\) ";
+      style = "bright-black";
+    };
+    cmd_duration = {
+      format = "[$duration]($style) ";
+      style = "yellow";
+    };
+    python = {
+      format = "[$virtualenv]($style) ";
+      style = "bright-black";
+    };
   };
 
   services.openssh = {
