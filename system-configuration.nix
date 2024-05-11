55f899c5e7291e43e85f16d2a44d142999b9f652
vaaski <admin@vaa.ski>
2024-05-11 11:42:47 +0200

    add 1TB-SSD from hserver

--- system-configuration.nix
+++ system-configuration.nix
@@ -44,6 +44,16 @@
         in
         [ "${automount_opts},credentials=/home/o/nixos/smb-secrets" ];
     };
+    "/media/hserver-1TB-SSD" = {
+      device = "//192.168.88.100/1TB-SSD";
+      fsType = "cifs";
+      options =
+        let
+          automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+        in
+        [ "${automount_opts},credentials=/home/o/nixos/smb-secrets" ];
+    };
   };
 
   services.samba = {
